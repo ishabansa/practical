5300554ad2fd098cc7bd7fb036f809a3a901f836
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-02 15:55:00 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+
+public static void main(String args[]) {
+
+System.out.print ("" +9 + 7 + " ");
+
+System.out.print (9 + "" + 7 + " ");
+
+System.out.print (9+ 7 + "" + " ");
+
+System.out.print (9 + 7 + " ");
+
+}
+
+}
