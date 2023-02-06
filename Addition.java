1e9930a98d2b9474fc3af0a7567b981e2615a8a2
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-06 19:17:06 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,17 @@
+class Addition{
+
+public static void main(String []args){
+
+System.out.println("The two number is :");
+
+int a = 5;  // initialize 1st local variable
+
+int b = 6;  // initialize 2st local variable
+
+System.out.println(a +" "+ b);  // print line
+
+int sum = a+b; // logic
+
+System.out.println("The sum is = " + sum);//  print line for addition
+
+}}
