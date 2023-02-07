f209d2e55e1bd0ebadff946f4add806db548c95c
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-07 19:14:28 +0530

    Add files via upload

---
+++ Parameterizedcons.java
@@ -0,0 +1,21 @@
+package method;
+
+public class Parameterizedcons {
+	int age;
+	String name;
+	//parameterized cons where we have to pass the parameter / arguments
+	Parameterizedcons(int a, String n){
+		age=a;
+		name=n;
+		}
+	void show() {
+		System.out.println(age+ " "+name);}
+	public static void main(String[] args) {
+		Parameterizedcons d=new Parameterizedcons(50,"Karan");
+		Parameterizedcons d1=new Parameterizedcons(25,"Rohit");
+		Parameterizedcons d2=new Parameterizedcons(21,"Roma");
+		d.show();
+		d1.show();
+		d2.show();
+	}
+	}
