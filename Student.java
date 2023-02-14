3f1ae2f41f61639e477d5c5384b4444b563b976d
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-14 21:02:04 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,22 +1,23 @@
-class Student1{   //defining a student class
-int slno;        //defining field/ data member/ Instance variable
-String name;
+package Aggregation;
 
-}
-
-class Student{
-
-public static void main(String...args){    //method of student class
-
-Student1 s = new Student1();         // creating a object
-s.slno= 01;
-s.name ="isha";
-
-Student1 m = new Student1();         // creating a object
-m.slno= 02;
-m.name ="ishaa";
-
-System.out.println(s.slno+" "+s.name);
-System.out.println(m.slno+" "+m.name);
-}
-}
+public class Student {  //class 1
+	int id;
+	String name;
+	Address address; //aggretion (entity ref of address class)
+	public Student(int id, Address address, String name) {  //parameterized cons
+		this.id=id;
+		this.name=name;
+		this.address=address;
+	}
+	void display() {  //method
+		System.out.println(id+" "+name);
+		System.out.println(address.city+" "+address.state+" "+address.country);
+		}
+	public static void main(String[] args) {
+		Address address1 = new Address("dgp","wb","india");
+		Address address2 = new Address("kol","wb","india");
+		Student s=new Student(12,address1,"ISHA");
+		Student s1 =new Student(12,address2,"Anudip");
+		s.display();
+		s1.display();
+	}}
