ff9faa204725b751a01fdffd05c197c53dcf593b
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-17 17:43:36 +0530

    Add files via upload

---
+++ Unaryop.java
@@ -0,0 +1,10 @@
+class Unaryop{
+public static void main(String ...args){
+int a=5;
+//postfix & prefix for increment
+System.out.println(a++);
+System.out.println(++a);
+//postfix & prefix for decrement
+System.out.println(a--);
+System.out.println(--a);
+}}
