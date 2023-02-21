bc6b1a0688dfac5f6e793ce7c7843aaf41384500
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-02-21 20:11:49 +0530

    Add files via upload

---
+++ ForEachArr.java
@@ -0,0 +1,14 @@
+package controlstatement;
+
+public class ForEachArr {
+	public static void main(String[] args) {
+		// declare an array
+		int arr[]= {12,22,45,25,32};
+		//using for each loop for print the array
+		for(int i:arr) {
+			System.out.println(i);
+			
+		}
+	}
+
+}
