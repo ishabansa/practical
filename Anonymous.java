ab034a27ed50f0086f26130c3c9f86691accea5c
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-03-03 19:25:51 +0530

    Add files via upload

---
+++ Anonymous.java
@@ -0,0 +1,10 @@
+package Array;
+//no need to declare the array inside the method
+public class Anonymous {
+	static void print(int arr[]) { //static print method receives an array as a arguments / parameterised
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+	public static void main(String[] args) {
+		print(new int[] {10,20,30,40,50,70,80}); //passing anonymous array to method print
+	}}
