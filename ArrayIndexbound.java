bbb2c71a4a36ac29e9e7d8977d8cb2fd2981be4a
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-03-03 19:26:11 +0530

    Add files via upload

---
+++ ArrayIndexbound.java
@@ -0,0 +1,9 @@
+package Array;
+public class ArrayIndexbound {
+public static void main(String[] args) {
+	int a[]= {2,5,4,8,9};
+	for(int i=0; i<a.length;i++) {
+		System.out.println(a[i]);
+	}
+}
+}
