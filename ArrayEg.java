46b5817c2fe2d9933f3ccc3edceffa27cca3bf1e
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-03-04 19:28:35 +0530

    Add files via upload

---
+++ ArrayEg.java
@@ -0,0 +1,17 @@
+package Array;
+// example of single dimensional array
+public class ArrayEg {
+	public static void main(String[] args) {
+		String [] names = {"Mamta","Ram","Karan","Ajay","Ajith","Tushar"}; //declaring & assigning
+		for (int i=0;i<names.length;i++) {
+			System.out.println(names[i]);
+		}
+		System.out.println();
+		int [] phone = {123,456,567,789,955,333,555,111,2345,7890};
+		for (int j=0;j<phone.length;j++) {
+			System.out.println(phone[j]);
+			
+		}
+	}
+
+}
