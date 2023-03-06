641baea440eda36f5f51057e1aeff3d206271c3e
ishabansa <122788226+ishabansa@users.noreply.github.com>
2023-03-06 16:48:31 +0530

    Add files via upload

---
+++ AdditionArray_1.java
@@ -0,0 +1,21 @@
+package Array;
+//two matrices addition
+public class AdditionArray_1 {
+	public static void main(String[] args) {
+		// create matrix
+		int a [][]= {{1,2},{3,4}};
+		int b [][]= {{1,5},{3,5}};
+		//for storing result creating another matrix
+		int c[][]=new int [2][2]; //two row two column
+		//logic and printing
+		for(int i=0;i<2;i++) {//printing row
+			for(int j=0;j<2;j++) {//printing column
+				c[i][j]=a[i][j]+b[i][j];
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+	}
+	//two matrix multiplication in array
+
